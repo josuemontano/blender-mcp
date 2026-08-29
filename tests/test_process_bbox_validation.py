3659870e3ca37e7453880e32761b5b1c4afb1459
Josue Montano <josuemontanoa@gmail.com>
2026-08-28 22:53:15 -0400

    chore: move test file to the tests folder

--- tests/test_process_bbox_validation.py
+++ tests/test_process_bbox_validation.py
@@ -1,6 +1,6 @@
 import pytest
 
-from blender_mcp.server import _process_bbox
+from blender_mcp.server.tools.hyper3d import _process_bbox
 
 
 @pytest.mark.parametrize("bbox", ([0, 1, 1], [-1, 1, 1]))
